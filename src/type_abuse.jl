2ff3d68e2a3954e54d9f6cf242f48bf0410780ef
nwamsley1 <n.t.wamsley@wustl.edu>
2023-03-11 13:32:53 -0600

    passing all tests)

--- src/type_abuse.jl
+++ src/type_abuse.jl
@@ -64,5 +64,20 @@ function calculate_branch(numbers::AbstractVector{<:Real}, operations::AbstractS
     # iterate through the characters in `operations` (which will all be one of `+`, `-`, `*`, `/`)
     # Do not create an Operator from the character, instead just use a set of `if...elseif...end`
     # conditionals.
-    error("you have to implement this function")
+    a = Float64(numbers[1])
+    for i = 1:length(operations)
+        b = numbers[i+1]
+        op = operations[i]
+        if op == '+'
+            a = a+b
+        elseif op == '/'
+            a = a/b
+        elseif op == '*'
+            a = a*b
+        elseif op == '-'
+            a = a-b
+        end
+    end
+    a
+    #error("you have to implement this function")
 end
